a570cc52fcbf81298010c4c083332dcd8516b0d2
Soukupis <joseph.soukup@outlook.com>
2020-12-16 13:32:55 +0100

    controller

---
+++ api/Controllers/MainController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    public class MainController
+    {
+
+        private static readonly string[] Summaries = new[]
+        {
+            "MIC", "TEMP", "PHOTO", "TRIM1", "TRIM2", "TRIM3"
+        };
+
+        [HttpGet]
+        public Task<string[]> Get()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            return ports;
+        }
+    }
+}
